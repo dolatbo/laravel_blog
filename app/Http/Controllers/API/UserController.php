671b6388dc14b5554a9c300cf0809e0d58fa6798
dolatbo <dolatbo@gmail.com>
2020-03-02 09:09:36 -0300

    Autenticação

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+
+    private $tokenName = 'devsApp';
+
+    public function login()
+    {
+        $email = request('email');
+        $password = request('password');
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            $user = Auth::user();
+            $sucesso['token'] = $user->createToken($this->tokenName)->accessToken;
+            return response()->json(['ok' => $sucesso]);
+        }
+        return response()->json(['error' => 'Não autorizado'], 401);
+    }
+
+    public function registrar(Request $request)
+    {
+        $validarUsuario = Validator::make($request->all(), ['name' => 'requerid', 'email' => 'required|email', 'password' => 'required', 'c_password' => 'required|same:password']);
+        if ($validarUsuario->fails()) {
+            return response()->json(['error' => $validarUsuario->errors()], 400);
+        }
+        $json = $request->all();
+        $json['password'] = bcrypt($json['password']); // Criptografar senha obrigatório
+        $user = User::create($json);
+
+        $sucesso['token'] = $user->createToken($this->tokenName)->accessToken;
+        $sucesso['id'] = $user->id;
+        $sucesso['nome'] = $user->name;
+
+        return response()->json(['ok' => $sucesso]);
+    }
+
+    public function detalhesUser()
+    {
+        $user = Auth::user();
+        return response()->json(['ok' => $user]);
+    }
+}
